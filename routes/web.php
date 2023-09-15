5bf1f51e4dfa6406d50554a2ed0fbfbe20752416
Amarangi Karunathilaka <amarangikarunathilaka@gmail.com>
2023-09-15 22:59:37 +0530

    initialize laravel project

---
+++ routes/web.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/index', function () {
+    return view('index');
+});
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('/registration', function () {
+    return view('registration');
+});
+
+Route::get('/sregister', function () {
+    return view('sregister');
+});
+
+Route::get('/terms', function () {
+    return view('terms');
+});
+
+Route::get('/tregister', function () {
+    return view('tregister');
+});
+
