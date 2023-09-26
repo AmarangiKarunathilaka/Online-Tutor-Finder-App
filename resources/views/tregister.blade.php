41673c62ed0e587b7b2308e145c924560bc81680
KavindraHerath <rekhankavindra@gmail.com>
2023-09-26 10:25:45 +0530

    tutor registration form created

---
+++ resources/views/tregister.blade.php
@@ -0,0 +1,208 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <meta name="description" content="">
+       
+        <title>Registation</title>
+
+        <!-- CSS FILES -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
+        <link href="https://fonts.googleapis.com/css2?family=League+Spartan:wght@100;300;400;600;700&display=swap" rel="stylesheet">
+
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+
+        <link href="css/bootstrap-icons.css" rel="stylesheet">
+
+        <link href="css/owl.carousel.min.css" rel="stylesheet">
+
+        <link rel="stylesheet" href="css/registrationstyle.css">
+        
+<!--
+
+Tooplate 2133 Moso Interior
+
+https://www.tooplate.com/view/2133-moso-interior
+
+Bootstrap 5 HTML CSS Template
+
+-->
+    </head>
+    
+    <body>
+
+        <nav class="navbar navbar-expand-lg  fixed-top shadow-lg">
+           
+            <div class="container">
+                
+                <img src="images/f.png" alt="" />
+
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav ms-auto">
+                        
+                        <li class="nav-item">
+                            <a class="nav-link click-scroll" href="index.html#section_1">Home</a>
+                        </li>
+                    
+                        <li class="nav-item">
+                            <a class="nav-link click-scroll" href="index.html#section_2">About</a>
+                        </li>
+        
+                        <li class="nav-item">
+                            <a class="nav-link click-scroll" href="index.html#section_4">Contact Us</a>
+                        </li>
+                    
+                        <li class="nav-item">
+                            <a class="nav-link click-scroll" href="header.html#section_5">Login</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link click-scroll" href="registration.html"><b>Register</b></a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+<section class="hero-section hero-slide d-flex justify-content-center align-items-center">
+    <div class="row justify-content-center">
+    <div class="form-box1">
+    <div class="container mt-4 ">
+           
+        <div class="form">
+            
+            <h1><div class="row justify-content-center"> Registration Form </div></h1>
+
+                <table>
+                    <form method="post">
+                        <tr>
+                            <div class="mt-4">
+                                <td><label for="TR-name">Name :</label></td>
+                                <td><input type="text" class="form-control" placeholder="First Name" name="name" required></td>
+                                <td><input type="text" class="form-control" placeholder="Last Name" name="name" required></td>
+                            </div>
+                        </tr>
+                </table>
+                        
+                        <div class="row">
+                            <div class="form-group col-4">
+                                <label for="TR-contact">Contact Number: </label>
+                            </div>
+                            <div class="form-group col-5">
+                                <input type="text" class="form-control" placeholder="+94" name="contact" required><br>
+                            </div>
+                        </div>
+                        
+                    
+
+                        
+                        <div class="TutorReg-1">
+                                <label for="TR-Qualification"> Qualifications :</label>
+                                <textarea type="text" class="form-control" placeholder="" name="qualification" rows="6" cols="23" required></textarea>
+                        </div>
+                        
+                <table>
+                    <tr>
+                        <tr>
+                            <div class="TutorReg-1">
+                                <td><label for="TR-subject"> Subject :</label></td>
+                                <td><input type="checkbox" class="TR-input"  name="subject1">
+                                <label for="TR-subject1"> Mathematics </label></td>
+                            </div>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td><input type="checkbox" class="TR-input"  name="subject2">
+                                <label for="TR-subject2"> Biology </label>
+                            </td>
+                        <tr>
+                        <tr>
+                            <td></td>
+                            <td><input type="checkbox" class="TR-input"  name="subject3">
+                                <label for="TR-subject3"> Chemistry </label>
+                        </td>
+                        <tr>
+                            <td></td>
+                            <td><input type="checkbox" class="TR-input"  name="subject4">
+                                <label for="TR-subject4"> Physics </label>
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <tr>
+                                <div class="TutorReg-1">
+                                    <td><label for="TR-medium"> Medium :</label></td>
+                                    <td><input type="checkbox" class="TR-input" name="medium1">
+                                    <label for="TR-medium1"> Sinhala </label></td>
+                                </div>
+                            </tr>
+                            <tr>
+                                <td><td><input type="checkbox" class="TR-input" name="medium2">
+                                    <label for="TR-medium2"> English </label></td></td>
+                            <tr>
+                            <tr>
+                                <td><td><input type="checkbox" class="TR-input" name="medium3">
+                                    <label for="TR-medium3"> Tamil </label></td></td>
+                            <tr>
+                        </tr>
+                        
+                        <tr>
+                            <div class="TutorReg-1">
+                                <td><label for="TR-email"> E-mail :</label></td>
+                                <td><input type="email" class="form-control" placeholder="Enter University E-mail" name="email" required></td>
+                            </div>
+                        </tr>
+
+                        <tr>
+                            <div class="TutorReg-1">
+                                <td><label for="TR-password"> Password :</label></td>
+                                <td> <input type="password" class="form-control" placeholder="Enter Password" name="password"></td>
+                            </div>
+                        </tr>
+                        <tr>
+                            <div class="TutorReg-1">
+                                <td><label for="TR-password"> Confirm Password :</label></td>
+                                <td> <input type="password" class="form-control" placeholder="Re-enter Password" name="password"></td><br>
+                            </div>
+                        </tr>
+                    </tr>
+                </table>
+                            <div class="row">
+                            <div class="form-group col-1">
+                                <input type="checkbox" class="TR-input" name="terms" required>
+                                </div>
+                                <div class="form-group col-11">
+                                <label for="TR-terms"> I Agree to the <a href="terms.html" style="color: blue"> Terms and Conditionss </a>  </label>
+                            </div>
+                        </div>
+                        
+                        
+                        <button type="submit" name="submit" class="btn" style="font-size: 20px; text-align:center; width: 70%"> Register </button>
+                        
+        
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+ <!-- JAVASCRIPT FILES -->
+ <script src="js/jquery.min.js"></script>
+ <script src="js/bootstrap.min.js"></script>
+ <script src="js/click-scroll.js"></script>
+ <script src="js/jquery.backstretch.min.js"></script>
+ <script src="js/owl.carousel.min.js"></script>
+
+</body>
+</html>
+
+
