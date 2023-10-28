1db7217e72703043ce32743ab9a4efec14c89ce9
Amarangi Karunathilaka <amarangikarunathilaka@gmail.com>
2023-10-28 18:31:03 +0530

    Update student.blade.php

--- resources/views/student.blade.php
+++ resources/views/student.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
   <meta charset="UTF-8" />
-  <title>Dashboard | By Code Info</title>
+  <title>Dashboard</title>
   <link rel="stylesheet" href="css/student.css" />
   <link rel="stylesheet" href="css/dashboard.css">
 
