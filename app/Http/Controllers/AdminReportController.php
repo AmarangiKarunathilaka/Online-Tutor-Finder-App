af50f3e3c9ae967567346685786557b2b9c4c52c
RamalPerera <ramalrangika1997@gmail.com>
2023-11-29 15:02:36 +0530

    Admin Report controller created

--- app/Http/Controllers/AdminReportController.php
+++ app/Http/Controllers/AdminReportController.php
@@ -1,5 +1,44 @@
-<!--
-class YourControllerName extends Controller {
+// app/Http/Controllers/ReportController.php
+<?php
+
+use PDF;
+use App\Models\studentRegister;
+use App\Models\tutorRegister;
+use App\Models\Feedback;
+
+class AdminReportController extends Controller {
+   
+    public function downloadStudentListPDF()
+    {
+        $students = studentRegister::all();
+
+        $pdf = PDF::loadView('reports.student_list_pdf', compact('students'));
+
+        return $pdf->download('student_list.pdf');
+    }
+
+    public function downloadTutorListPDF()
+    {
+        $tutors = tutorRegister::all();
+
+        $pdf = PDF::loadView('reports.tutor_list_pdf', compact('tutors'));
+
+        return $pdf->download('tutor_list.pdf');
+    }
+
+    public function downloadFeedbackCountPDF()
+    {
+        $feedbackCount = Feedback::count();
+
+        $pdf = PDF::loadView('reports.feedback_count_pdf', compact('feedbackCount'));
+
+        return $pdf->download('feedback_count.pdf');
+    }
+
+}
+
+
+/*class YourControllerName extends Controller {
     public function exportTableToPDF() {
         
         $data = YourModelName::all();
@@ -13,5 +52,5 @@ class YourControllerName extends Controller {
         // Download the PDF file
         return $pdf->download('table_export.pdf');
     }
-}
--->
+}*/
+
