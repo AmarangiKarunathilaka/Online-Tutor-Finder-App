4fd852872682d178fed0906ce3aaeba3b69d86e4
witharana99 <sathsaraniishara80@gmail.com>
2023-12-07 11:04:48 +0530

    Code setup
    Code setup

--- app/Http/Controllers/ClassMaterialController.php
+++ app/Http/Controllers/ClassMaterialController.php
@@ -5,84 +5,15 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\ClassMaterial;
 use Illuminate\Support\Facades\Stroage;
-/*use App\Http\Controllers\Controller;
-use Illuminate\Support\Facades\DB;*/
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
 
 class ClassMaterialController extends Controller
 {
-    /*public function upload(Request $request)
+    
+    public function classMaterials()
     {
-        // Validate the request input
-        $request->validate([
-            'tutorName' => 'required',
-            'email' => 'required|email',
-            'subject' => 'required',
-            'title' => 'required',
-            'lecNote' => 'required',
-            'file' => 'required|mimes:pdf,doc,docx',
-        ]);
-
-        // Upload the file
-        $file = $request->file('file');
-        $fileName = $file->getClientOriginalName();
-        $file->storeAs('public/uploads', $fileName);
-
-          // Return a success message
-        return response()->json(['message' => 'Class material uploaded successfully!']);
-    }*/
-
-
-        // Store the material data in the database
-    public function upload(Request $request)
-     {
-        $classMaterial = new ClassMaterial();
-        $classMaterial->tutorName = $request->tutorName;
-        $classMaterial->email = $request->email;
-        $classMaterial->subject = $request->subject;
-        $classMaterial->title = $request->title;
-        $classMaterial->lecNote = $request->lecNote;
-        $classmaterial->file = $request->file;
-        $classmaterial->tutor_id = $request->key;
-
-        $file=$request->file;
-
-        if($file)
-        {
-            $filename=time().'.'.$file->getClientOriginalExtension();
-            $request->file->move('filestore',$filename);
-
-            $classmaterial->file=$filename;
-        }
-        $classmaterial->save();
-        return redirect()->back();
-       
-
-     }
-    public function classMaterials(){
         return view('upload-class-material');
-    }
-
-    public function uploadClassMaterialInput(Request $request)
-    {
-        
-        $keyValue = $request->input('key');
-
-         
-
-        ClassMaterial::create([
-        'tutor_id' => $request->key,
-        'tutorName' => $request->tutorName,
-        'email' => $request->email,
-        'subject' => $request->subject,
-        'title' => $request->title,
-        'lecNote' => $request->lecNote,
-        'file' => $request->material,
-        
-          
-
-    ]);
-
-    return redirect() -> back();
     }
 
     
@@ -111,35 +42,18 @@ class ClassMaterialController extends Controller
         return redirect()->back();
     }
 
-public function download(Request $request,$file)
-{
-    return response()->download(public_path('filestore/'.$file));
-}
+
+
+    public function adminClassMaterialList()
+    {
+        $classmaterial = ClassMaterial::all();
+        return view('adminClassMaterialList', compact('classmaterial'));
+    }
 
 
 
-public function adminClassMaterialList()
-{
-    $classmaterial = ClassMaterial::all();
-    return view('adminClassMaterialList', compact('classmaterial'));
-}
 
-
-public function materialDisplay()
-{
-    $classmaterial = ClassMaterial::where('status','=','active')->get();
-    return view('classMaterial', compact('classmaterial'));
-   
-}
-
-
-public function materialcontent()
-{
-    $classmaterial = ClassMaterial::where('status','=','active')->get();
-    return view('materialContent', compact('classmaterial'));
-}
-
-public function accept_material($id)
+    public function accept_material($id)
     {
         $data = ClassMaterial::find($id);
         $data->status = 'active';
@@ -148,7 +62,9 @@ public function accept_material($id)
         return redirect()->route('adminClassMaterialList')->with('success', 'Class Material accepted successfully!');
     }
 
-public function reject_material($id)
+
+
+    public function reject_material($id)
     {
         $data = ClassMaterial::find($id);
         $data->status = 'rejected';
@@ -157,5 +73,19 @@ public function reject_material($id)
         return redirect()->route('adminClassMaterialList')->with('success', 'Class Material rejected successfully!');
     }
 
+    
+
+    public function materialcontent()
+    {
+        $classmaterial = ClassMaterial::where('status','=','active')->get();
+        return view('materialContent', compact('classmaterial'));
+    }
+
+
+
+    public function download(Request $request,$file)
+    {
+         return response()->download(public_path('filestore/'.$file));
+    }
 
 }
