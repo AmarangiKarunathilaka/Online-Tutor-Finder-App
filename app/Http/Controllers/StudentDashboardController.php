05c05161769175f698a0895771419e5477e77443
Amarangi Karunathilaka <amarangikarunathilaka@gmail.com>
2023-12-10 21:18:44 +0530

    student dashboard advertisement display subject wise done

--- app/Http/Controllers/StudentDashboardController.php
+++ app/Http/Controllers/StudentDashboardController.php
@@ -10,8 +10,23 @@ class StudentDashboardController extends Controller
     public function advertisementDisplay()
     {
     
-        $advertisements = Advertisement::where('status','=','accepted')->get();
-        return view('studentDashboard', compact('advertisements'));
+        $advertisementMaths = Advertisement::where('status', '=', 'accepted')
+        ->where('subject', '=', 'Mathematics')
+        ->get();
+
+        $advertisementPhysics = Advertisement::where('status', '=', 'accepted')
+        ->where('subject', '=', 'Physics')
+        ->get();
+
+        $advertisementChemistry = Advertisement::where('status', '=', 'accepted')
+        ->where('subject', '=', 'Chemistry')
+        ->get();
+
+        $advertisementBiology = Advertisement::where('status', '=', 'accepted')
+        ->where('subject', '=', 'Biology')
+        ->get();
+        
+        return view('studentDashboard', compact('advertisementMaths', 'advertisementPhysics', 'advertisementChemistry', 'advertisementBiology'));
         
     }
 }
--- resources/views/studentdashboard.blade.php
+++ resources/views/studentdashboard.blade.php
@@ -27,108 +27,20 @@
     <title>Dashboard Sidebar Menu</title> 
 </head>
 <body>
-    <nav class="sidebar close">
-        <header>
-            <div class="image-text">
-                <span class="image">
-                    <img src="images/f.png" alt="">
-                </span>
-
-                <div class="text logo-text">
-
-
-                    <span class="name">Tutor</span>
-                    <span class="profession">Finder System</span>
-
-
-                </div>
-            </div>
-
-            <i class='bx bx-chevron-right toggle'></i>
-        </header>
-
-        <div class="menu-bar">
-            <div class="menu">
-
-                <li class="search-box">
-                    <i class='bx bx-search icon'></i>
-                    <input type="text" placeholder="Search...">
-                </li>
-
-                <ul class="menu-links">
-                    <li class="nav-link">
-                        <a href="#">
-                            <i class='bx bx-home-alt icon' ></i>
-                            <span class="text nav-text">Dashboard</span>
-                        </a>
-                    </li>
-
-                    <li class="nav-link">
-                        <a href="#">
-                            <i class='bx bx-note icon' ></i>
-                            <span class="text nav-text">Class Registration</span>
-                        </a>
-                    </li>
-
-                    <li class="nav-link">
-                        <a href="/classMaterial">
-                            <i class='bx bx-book-open icon'></i>
-                            <span class="text nav-text">Class Materials</span>
-                        </a>
-                    </li>
-
-                    <li class="nav-link">
-                        <a href="/chat">
-                            <i class='bx bxs-conversation icon' ></i>
-                            <span class="text nav-text">Conversation</span>
-                        </a>
-                    </li>
-
-                    <li class="nav-link">
-                        <a href="#">
-                            <i class='bx bx-edit icon' ></i>
-                            <span class="text nav-text">Feedback</span>
-                        </a>
-                    </li>
-
-                </ul>
-            </div>
-
-            <div class="bottom-content">
-                <li class="">
-                    <a href="/">
-                        <i class='bx bx-log-out icon' ></i>
-                        <span class="text nav-text">Logout</span>
-                    </a>
-                </li>
-
-                <li class="mode">
-                    <div class="sun-moon">
-                        <i class='bx bx-moon icon moon'></i>
-                        <i class='bx bx-sun icon sun'></i>
-                    </div>
-                    <span class="mode-text text">Dark mode</span>
-
-                    <div class="toggle-switch">
-                        <span class="switch"></span>
-                    </div>
-                </li>
-                
-            </div>
-        </div>
-
-    </nav>
-
-    <section class="home">
-
-       
-
+@extends('studentHomeContent')
+  @section('content')
+  <section class="home">
+        <div class="text"></div>
+    
+    
+    <div class="container">
+    
         
    
 
     
 <!---------------------------------------
-    advertisement   (ishara)           
+    advertisements            
   ----------------------------------------->
 
     <section class= "advertisement">
@@ -139,177 +51,101 @@
     </section>
 
 
-<section class="content">
+    <section class="content">
 
 
-     <div class="col-lg-12 col-12">
-        <div class="subject">
-            <h3><i><u>Combine Maths</u></i></h3>
+        <div class="col-lg-12 col-12">
+            <div class="subject">
+                <h3><i><u>Mathematics</u></i></h3>
+            </div>
         </div>
-    </div>
 
-<div class="row">
+        <div class="row">
 
-<div class="owl-carousel reviews-carousel">
-                        @foreach ($advertisements as $advertisement)
+            <div class="owl-carousel reviews-carousel">
+                    @foreach ($advertisementMaths as $advertisementMath)
                         <div class="col">   
-                        
-                        <img src="/uploads/{{ $advertisement ->photo }}"  width='50' height='50' class="img img-responsive"/>
-                       
+                                    
+                            <img src="/uploads/{{ $advertisementMath ->photo }}"  width='50' height='50' class="img img-responsive"/>
+                            <h1>{{ $advertisementMath->tutorName }}</h1>
                         </div>
-                        @endforeach
-                    </div>
-
-</div>
-</div>
-
-
-<div class="col-lg-12 col-12">
-    <div class="subject">
-        <h3><i><u>Biology</u></i></h3>
-    </div>
-</div> 
-
-<div class="row">
-    
-    <div class="owl-carousel reviews-carousel">
-
-        <div class="col">
-            <img src="images/team-01.jpg">
-                <h2>James Fernando </h2>
-                    <p> Experience: 5 years<br>
-                    Rate: $50/hour<br>
-                    <a href="#">Contact</a></p>
-    
-        </div>
-
-
-        <div class="col">
-            <img src="images/team-01.jpg">
-                <h2>James Fernando </h2>
-                <p> Experience: 5 years<br>
-                Rate: $50/hour<br>
-                <a href="#">Contact</a></p>
-        </div>
-
-
-        <div class="col">
-            <img src="images/team-01.jpg">
-                <h2>James Fernando </h2>
-                    <p> Experience: 5 years<br>
-                    Rate: $50/hour<br>
-                    <a href="#">Contact</a></p>
-        </div>
-
-
-        <div class="col">
-            <img src="images/team-01.jpg">
-                <h2>James Fernando </h2>
-                    <p> Experience: 5 years<br>
-                    Rate: $50/hour<br>
-                    <a href="#">Contact</a></p>
-        </div>
-    
-    </div>
-</div> 
-
-
-
-    <div class="col-lg-12 col-12">
-        <div class="subject">
-        <h3><i><u>Chemistry</u></i></h3>
-    </div>
-</div> 
-
-<div class="row">
-
-    <div class="owl-carousel reviews-carousel">
-
-    <div class="col">
-        <img src="images/team-01.jpg">
-            <h2>James Fernando </h2>
-                <p> Experience: 5 years<br>
-                Rate: $50/hour<br>
-                <a href="#">Contact</a></p>
-</div>
-
-<div class="col">
-    <img src="images/team-01.jpg">
-        <h2>James Fernando </h2>
-            <p> Experience: 5 years<br>
-            Rate: $50/hour<br>
-            <a href="#">Contact</a></p>
-</div>
-
-<div class="col">
-    <img src="images/team-01.jpg">
-        <h2>James Fernando </h2>
-            <p> Experience: 5 years<br>
-            Rate: $50/hour<br>
-            <a href="#">Contact</a></p>
-</div>
-
-<div class="col">
-    <img src="images/team-01.jpg">
-        <h2>James Fernando </h2>
-            <p> Experience: 5 years<br>
-            Rate: $50/hour<br>
-            <a href="#">Contact</a></p>
-</div>
-
-</div>
-</div>
-
-
-<div class="col-lg-12 col-12">
-    <div class="subject">
-        <h3><i><u>Physics</u></i></h3>
-    </div>
-</div> 
-
-<div class="row">
-    <div class="owl-carousel reviews-carousel">
-
-        <div class="col">
-            <img src="images/team-01.jpg">
-                <h2>James Fernando </h2>
-                <p> Experience: 5 years<br>
-                Rate: $50/hour<br>
-                <a href="#">Contact</a></p>
+                    @endforeach
+            </div>
 
         </div>
 
-    <div class="col">
-        <img src="images/team-01.jpg">
-            <h2>James Fernando </h2>
-                <p> Experience: 5 years<br>
-                Rate: $50/hour<br>
-                <a href="#">Contact</a></p>
+
+
+        <div class="col-lg-12 col-12">
+            <div class="subject">
+                <h3><i><u>Physics</u></i></h3>
+            </div>
+        </div> 
+
+        <div class="row">
+            
+            <div class="owl-carousel reviews-carousel">
+                    @foreach ($advertisementPhysics as $advertisementPhysic)
+                            <div class="col">   
+                                
+                                <img src="/uploads/{{ $advertisementPhysic ->photo }}"  width='50' height='50' class="img img-responsive"/>
+                                <h1>{{ $advertisementPhysic->tutorName }}</h1>
+                            </div>
+                    @endforeach
+            </div>
+            
+        </div> 
+
+
+
+        <div class="col-lg-12 col-12">
+            <div class="subject">
+                <h3><i><u>Chemistry</u></i></h3>
+            </div>
+        </div> 
+
+        <div class="row">
+
+            <div class="owl-carousel reviews-carousel">
+                    @foreach ($advertisementChemistry as $advertisementChem)
+                            <div class="col">   
+                                
+                                <img src="/uploads/{{ $advertisementChem ->photo }}"  width='50' height='50' class="img img-responsive"/>
+                                <h1>{{ $advertisementChem ->tutorName }}</h1>
+                            </div>
+                    @endforeach
+            </div>
+
+        </div>
+
+
+        <div class="col-lg-12 col-12">
+            <div class="subject">
+                <h3><i><u>Biology</u></i></h3>
+            </div>
+        </div> 
+
+        <div class="row">
+            <div class="owl-carousel reviews-carousel">
+                    @foreach ($advertisementBiology as $advertisementBio)
+                            <div class="col">   
+                                
+                                <img src="/uploads/{{ $advertisementBio ->photo }}"  width='50' height='50' class="img img-responsive"/>
+                                <h1>{{ $advertisementBio ->tutorName }}</h1>
+                            </div>
+                    @endforeach
+            </div>
+
+        </div>
+
+    </section>
     </div>
-
-<div class="col">
-    <img src="images/team-01.jpg">
-        <h2>James Fernando </h2>
-            <p> Experience: 5 years<br>
-            Rate: $50/hour<br>
-            <a href="#">Contact</a></p>
-</div>
-
-<div class="col">
-    <img src="images/team-01.jpg">
-        <h2>James Fernando </h2>
-            <p> Experience: 5 years<br>
-            Rate: $50/hour<br>
-            <a href="#">Contact</a></p>
-</div>
-</div>
-</div>
-</section>
+    @endsection
 </section> 
 
-</section>
 
-<script src="js/script.js"></script>
+
+
 
 
     <script src="js/jquery.min.js"></script>
