96f37e4d3fc361eb4ec59abc83f67333f65e775b
witharana99 <sathsaraniishara80@gmail.com>
2023-12-12 12:35:24 +0530

    Edit tutor profile created
    Edit tutor profile created

--- resources/views/editTutorProfile.blade.php
+++ resources/views/editTutorProfile.blade.php
@@ -5,6 +5,7 @@
 
 
 <title>edit Tutor Profile</title>
+<meta name="csrf-token" content="{{ csrf_token() }}">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet">
 
@@ -19,6 +20,10 @@
         <div class="text">Edit Profile</div>
 <div class="container-xl  mt-4">
 
+<form action="{{ url('/TutorprofileInput') }}" method="post"  enctype="multipart/form-data">
+            @csrf
+
+            <input type="hidden" name="key" value="1">
 <hr class="mt-0 mb-4">
 <div class="row">
 <div class="col-xl-4">
@@ -28,13 +33,17 @@
         <div class="card-body text-center">
             <div class="hero">
                 
-                    
-                    <img class="img-account-profile rounded-circle mb-2" src="images/f.png" alt>
+            @foreach ($profile as $profile)
+                          
+                          <img  src="/profileImage/{{ $profile ->image }}" width='150' height='150' class="img-account-profile rounded-circle mb-2" alt/>
+                                 
+                          @endforeach
+                   
 
                     <div class="col-xl-6">
                     <lable for="input-file"><b>Upload image:</b></div>
-                    <input type="file" accept="image/jpeg, image/png, image/jpg" id="input-file">
-                    <div class="back" ><a href="/adminClassMaterialList"> <b>Upload</b></a></div></lable>
+                    <input type="file" name="image" accept="image/jpeg, image/png, image/jpg" id="input-file"></lable>
+                    <div class="back" style= " margin-top:40px; margin-bottom:40px;"><a href="#"> <b>Update</b></a></div>
             
             </div>
             <script>
@@ -62,17 +71,29 @@
     <thead>
         <tr>
             <th></th>
-            <th>Monday</th>
+            @foreach ($days as $day)
+                        <th>{{ $day }}</th>
+                    @endforeach
+             <!--<th>Monday</th>
             <th>Tuesday</th>
             <th>Wednesday</th>
             <th>Thursday</th>
             <th>Friday</th>
             <th>Saturday</th>
-            <th>Sunday</th>
+            <th>Sunday</th>-->
         </tr>
     </thead>
     <tbody>
-        <tr>
+
+    @foreach ($timeSlots as $timeSlot)
+                    <tr>
+                        <td>{{ $timeSlot }}</td>
+                        @foreach ($days as $day)
+                            <td class="timetable-cell" data-day="{{ $day }}" data-time-slot="{{ $timeSlot }}"></td>
+                        @endforeach
+                    </tr>
+                @endforeach
+        <!--<tr>
             <td>8:00 AM - 10:00 AM</td>
             <td class="slot" data-day="monday" data-time="slot1"></td>
             <td class="slot" data-day="tuesday" data-time="slot1"></td>
@@ -111,16 +132,16 @@
             <td class="slot" data-day="friday" data-time="slot4"></td>
             <td class="slot" data-day="saturday" data-time="slot4"></td>
             <td class="slot" data-day="sunday" data-time="slot4"></td>
-        </tr>
+        </tr>-->
     </tbody>
 </table>
-<div class="back" style= "margin-left:40%; margin-top:40px; margin-bottom:40px;"><a href="/adminClassMaterialList"> <b>Update</b></a></div>
+<div class="back" style= "margin-left:40%; margin-top:40px; margin-bottom:40px;"><a href="#"> <b>Update</b></a></div>
             
 </div>
     </div>
-    </form>
             
-<script>
+            
+<!--<script>
     document.addEventListener("DOMContentLoaded", function() {
         var slots = document.querySelectorAll('.slot');
 
@@ -130,7 +151,48 @@
             });
         });
     });
+</script>-->
+<script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+
+<script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.9.0/fullcalendar.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+
+
+    <!-- Include this in your HTML template to generate the CSRF token -->
+
+
+<script>
+    $(document).ready(function () {
+        $('.timetable-cell').click(function () {
+            var day = $(this).data('day');
+            var timeSlot = $(this).data('time_slot');
+            var csrfToken = $('input[name=csrfmiddlewaretoken]').val();
+           
+            $.ajax({
+                url: '/update-timetable',
+                method: 'POST',
+                data: { day: day, timeSlot: timeSlot, csrfmiddlewaretoken: csrfToken },
+                success: function (response) {
+                    if (response.success) {
+                        alert('Cell updated successfully!');
+                    } else {
+                        alert('Failed to update cell.');
+                    }
+                },
+                error: function () {
+                    alert('Error updating cell.');
+                }
+            });
+        });
+    });
 </script>
+
 
 
 
@@ -138,8 +200,8 @@
 
 <div class="container">
 <h1 class="form-title">Profile Details</h1>
-      <form action="{{ url('/classMaterialInput') }}" method="post"  enctype="multipart/form-data">
-            @csrf
+      <!--<form action="{{ url('/classMaterialInput') }}" method="post"  enctype="multipart/form-data">
+            @csrf-->
      
             
         <div class="main-user-info">
@@ -147,14 +209,14 @@
             <label for="tutorName">Name</label>
             <input type="text"
                     id="tutorName"
-                    name="tutorName"
+                    name="tutorFullName"
                     placeholder="Enter Full Name"/>
           </div>
           <div class="user-input-box">
             <label for="email">Email</label>
             <input type="email"
                     id="email"
-                    name="email"
+                    name="tutorEmail"
                     placeholder="Enter Email"/>
           </div>
 
@@ -162,7 +224,7 @@
             <label for="num">Contact No.</label>
             <input type="text"
                     id="num"
-                    name="num"
+                    name="tutorPhoneNumber"
                     placeholder="+94"/>
           </div>
           
