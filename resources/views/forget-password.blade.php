757165ff45989315573bdb9318668e3af63e78e6
KavindraHerath <rekhankavindra@gmail.com>
2023-12-13 09:37:53 +0530

    update forget-password.blade.php

--- resources/views/forget-password.blade.php
+++ resources/views/forget-password.blade.php
@@ -1,12 +1,84 @@
-<!DOCTYPE html>
-<html>
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<head>
-    <!-- Your head content goes here -->
-</head>
+        <meta name="description" content="">
+        
+
+        <title>Registation</title>
+
+        <!-- CSS FILES -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
+        <link href="https://fonts.googleapis.com/css2?family=League+Spartan:wght@100;300;400;600;700&display=swap" rel="stylesheet">
+
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+
+        <link href="css/bootstrap-icons.css" rel="stylesheet">
+
+        <link href="css/owl.carousel.min.css" rel="stylesheet">
+
+        <link rel="stylesheet" href="css/registrationstyle.css">
+
+        <link rel="stylesheet" href="css/style.css">
+        
+<!--
+
+Tooplate 2133 Moso Interior
+
+https://www.tooplate.com/view/2133-moso-interior
+
+Bootstrap 5 HTML CSS Template
+
+-->
+    </head>
+    
+    <body>
+
+        <nav class="navbar navbar-expand-lg  fixed-top shadow-lg">
+           
+            <div class="container">
+                
+                <img src="images/f.png" alt="" />
+
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav ms-auto">
+
+                        
+                        <li class="nav-item">
+                            <a class="nav-link click-scroll" href="/">Home</a>
+                        </li>
+                    
+                        <li class="nav-item">
+                            <a class="nav-link click-scroll" href="/#section_2">About</a>
+                        </li>
+
+                    
+                        <li class="nav-item">
+                            <a class="nav-link click-scroll" href="/registration"><b>Register</b></a>
+
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+           
+        <section class="hero-section hero-slide d-flex justify-content-center align-items-center">
+
 
 <body>
-    <div>
+<section>
+        <div class="form-box" style="margin-top: 10%; height:400px;">
+            <div class="form-value text-center"  style="padding:25px">
+    
         <div class="mt-5">
             @if($errors->any())
             <div class="col-12">
@@ -25,16 +97,28 @@
             @endif
 
         </div>
+        <h3 class="mt-6">Password Reset</h3>
         <p>We will send a link to your email. Use that to reset the password</p>
         <form action="{{route('forget.password.post')}}" method="POST">
             @csrf
-            <div>
+            <div class="text-center mt-md-3 mt-2 mb-2 mb-md-3">
                 <label class="form-label">Email Address</label>
                 <input type="email" class="form-control" name="studentEmail">
             </div>
-            <button type="submit" class="btn btn-primary">Submit</button>
+            <button style="font-size: 15px;" type="submit" class="btn btn-primary">Submit</button>
         </form>
     </div>
-</body>
+    </section>
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
 
+
+ <!-- JAVASCRIPT FILES -->
+ <script src="js/jquery.min.js"></script>
+ <script src="js/bootstrap.min.js"></script>
+ <script src="js/click-scroll.js"></script>
+ <script src="js/jquery.backstretch.min.js"></script>
+ <script src="js/owl.carousel.min.js"></script>
+</body>
 </html>
+
