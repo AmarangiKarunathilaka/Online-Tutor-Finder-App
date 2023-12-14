cea4b30a5ff64348f9633cfe77dd849cb78b0291
RamalPerera <ramalrangika1997@gmail.com>
2023-12-14 12:34:42 +0530

    Button All
    Button All

--- resources/views/adminTutorList.blade.php
+++ resources/views/adminTutorList.blade.php
@@ -8,6 +8,10 @@
 
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="css/adminstyle.css">
+    <link href="css/report.css"  rel="stylesheet">
+
     <link rel="stylesheet" href="css/adminstyle.css">
     <title>Tutor Table</title>
 
