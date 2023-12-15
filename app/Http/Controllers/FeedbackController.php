8ad1688303aad6b078d782577d0f8c19e9412cb1
AkeshMT <akeshmt@gmail.com>
2023-12-15 11:55:07 +0530

    feedback controller update

--- app/Http/Controllers/FeedbackController.php
+++ app/Http/Controllers/FeedbackController.php
@@ -4,33 +4,37 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Feedback;
+use App\Models\WebsiteFeedback;
+use App\Models\TutorFeedback;
+use Illuminate\Support\Facades\Session;
+use App\Http\Controllers\LoginController;
 
 class FeedbackController extends Controller
-{
-    /*function websiteFeedbackForm(Request $req){
-        $feedback = new Feedback;
-        $feedback->name=$req->name;
-        $feedback->email=$req->email;
-        $feedback->rating=$req->rating;
-        $feedback->message=$req->message;
-        $feedback->save();
-    }*/
-
+{  
     public function feedback(){
         return view('websiteFeedbackForm');
     }
 
+    public function wfeedback(){
+        return view('websiteFeedback');
+    }
+    public function tfeedback(){
+        return view('tutorFeedback');
+    }
+
+    // tutor to website
     public function uploadFeedbackInput(Request $request){
 
-        $keyValue = $request->input('key');
+        $userId = Session::get('user_id');
 
-        Feedback::create([
-            'student_id'=> $request -> key,
-            'name'=> $request -> name,
-            'email'=> $request -> email,
-            'rating'=> $request -> rating,
-            'message'=> $request -> message,
-        ]);
+        $feedback = new Feedback();
+        $feedback -> tutor_id = $userId;
+        $feedback -> name = $request -> name;
+        $feedback -> email = $request -> email;
+        $feedback ->rating = $request -> rating;
+        $feedback ->  message= $request -> message;
+        
+        $feedback->save();
 
         return redirect() -> back();
     }
@@ -40,9 +44,13 @@ class FeedbackController extends Controller
                 
         // Fetch feedback data from the database
         $feedback = Feedback::all();
+        $wfeedback = WebsiteFeedback::all();
+        $tfeedback = TutorFeedback::all();
 
         // Return the admin feedback list view
         return view('adminFeedbackList', compact('feedback'));
+        return view('adminFeedbackList', compact('wfeedback'));
+        return view('adminFeedbackList', compact('tfeedback'));
     }
 
     public function feedbackDisplay()
@@ -61,7 +69,7 @@ class FeedbackController extends Controller
         $data->status = 'accepted';
         $data->save();
 
-        return redirect()->route('adminFeedbackList')->with('success', 'Feedback accepted successfully!');
+        return redirect() -> back();        
     }
 
     public function rejectFeedback($id)
@@ -70,12 +78,111 @@ class FeedbackController extends Controller
         $data->status = 'rejected';
         $data->save();
 
-        return redirect()->route('adminFeedbackList')->with('success', 'Feedback rejected successfully!');
+        return redirect() -> back();
+    }
+
+    // student to website
+    public function uploadwFeedbackInput(Request $request){
+
+        $userId = Session::get('user_id');
+
+        $wfeedback = new WebsiteFeedback();
+        $wfeedback -> student_id = $userId;
+        $wfeedback -> name = $request -> name;
+        $wfeedback -> email = $request -> email;
+        $wfeedback ->rating = $request -> rating;
+        $wfeedback ->  message= $request -> message;
+        
+        $wfeedback->save();
+        
+        return redirect() -> back();
     }
 
     
 
+    public function wfeedbackDisplay()
+    {
+    
+        $wfeedback = WebsiteFeedback::where('status','=','accepted')->get();
 
+        return view('index', compact('wfeedback'));
+        
 
+    }
+
+    public function acceptwFeedback($id)
+    {
+        $data = WebsiteFeedback::find($id);
+        $data->status = 'accepted';
+        $data->save();
+
+        return redirect() -> back();
+    }
+
+    public function rejectwFeedback($id)
+    {
+        $data = WebsiteFeedback::find($id);
+        $data->status = 'rejected';
+        $data->save();
+
+        return redirect() -> back();
+    }
+
+    // student to tutor
+    public function uploadtFeedbackInput(Request $request){
+
+        $userId = Session::get('user_id');
+
+        $tfeedback = new TutorFeedback();
+        $tfeedback -> student_id = $userId;
+        $tfeedback -> tutor = $request -> tutor;
+        $tfeedback -> name = $request -> name;
+        $tfeedback -> email = $request -> email;
+        $tfeedback ->rating = $request -> rating;
+        $tfeedback ->  message= $request -> message;
+        
+        $tfeedback->save();
+
+        return redirect() -> back();
+    }
+
+    
+    // this is on StudentDashboardController
+    // public function tfeedbackDisplay()
+    // {
+    
+    //     $tfeedback = TutorFeedback::where('status','=','accepted')->get();
+
+    //     return view('studentDashboard', compact('tfeedback'));
+        
+
+    // }
+
+    public function accepttFeedback($id)
+    {
+        $data = TutorFeedback::find($id);
+        $data->status = 'accepted';
+        $data->save();
+
+        return redirect() -> back();
+    }
+
+    public function rejecttFeedback($id)
+    {
+        $data = TutorFeedback::find($id);
+        $data->status = 'rejected';
+        $data->save();
+
+        return redirect() -> back();
+    }
+
+    public function combinedAdminDisplay()
+    {
+        $feedback = Feedback::all();
+        $wfeedback = WebsiteFeedback::all();
+        $tfeedback = TutorFeedback::all();
+
+        return view('adminFeedbackList', compact('feedback', 'wfeedback', 'tfeedback'));
+    }    
    
 }
