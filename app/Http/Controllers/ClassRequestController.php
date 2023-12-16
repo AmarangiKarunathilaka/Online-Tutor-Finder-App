2e4930ff40ecc0196e12b4db53f46d09fbbaf3b3
witharana99 <sathsaraniishara80@gmail.com>
2023-12-16 13:27:04 +0530

    Class request display
    Class request display

--- app/Http/Controllers/ClassRequestController.php
+++ app/Http/Controllers/ClassRequestController.php
@@ -6,6 +6,10 @@ use Illuminate\Http\Request;
 use App\Models\ClassRequest;
 use App\Models\Timetable;
 use App\Models\tutorRegister;
+use App\Models\tutorSubject;
+use App\Models\tutorMedium;
+use App\Models\tutordetail;
+use App\Models\studentRegister;
 use App\Http\controllers\Controller;
 use Illuminate\Support\Facades\Session;
 
@@ -18,9 +22,24 @@ class ClassRequestController extends Controller
 {
 
     public function classRequests(){
+
+        $userId = Session::get('user_id');                    
+        $detail = Tutordetail::where('tutordetails.tutor_id','=', $userId)
+                            ->get();
+
         $userId = Session::get('user_id');
         $tutorFullName= tutorRegister::where('tutor_registers.id','=', $userId)
                                 ->select('tutor_registers.tutorFullName')
+                                ->get();
+
+        $userId = Session::get('user_id');
+        $subject= tutorSubject::where('tutor_subjects.tutorSubject_id','=', $userId)
+                                ->select('tutor_subjects.tutorSubject')
+                                ->get();
+
+        $userId = Session::get('user_id');
+        $medium= tutorMedium::where('tutor_mediums.tutorMedium_id','=', $userId)
+                                ->select('tutor_mediums.tutorMedium')
                                 ->get();
 
         $userId = Session::get('user_id');
@@ -30,8 +49,10 @@ class ClassRequestController extends Controller
         $userId = Session::get('user_id');                        
         $days = Timetable::where('timetables.tutor_id','=', $userId)
                             ->distinct()->pluck('day');
+
+                        
                     
-        return view('classRequest', compact('timeSlots', 'days','tutorFullName'));
+        return view('classRequest', compact('timeSlots', 'days','tutorFullName','subject','medium','detail'));
         
     }
 
@@ -41,8 +62,8 @@ class ClassRequestController extends Controller
         $class= new ClassRequest();
         $class->tutor_id = $userId; 
         $class->tutorFullName=$request->input('tutorFullName'); 
-        $class->subject=$request->subject;
-        $class->medium=$request->medium; 
+        $class->subject=$request->input('tutorSubject'); 
+        $class->medium=$request->input('medium');  
         $class->day = $request->day;
         $class->time = $request->time;
         
@@ -54,15 +75,18 @@ class ClassRequestController extends Controller
       
         }
 
+       
 
         public function adminClassRequestList()
     {
                 
-        // Fetch requests data from the database
-        $requests = ClassRequest::all();
+        $userId = Session::get('user_id');
+        $studentName= studentRegister::where('student_registers.id','=', $userId)
+                                ->select('student_registers.studentFullName')
+                                ->get();
 
-        // Return the admin requests list view
-        return view('adminClassRequestList', compact('requests'));
+        $requests = ClassRequest::all();
+        return view('adminClassRequestList', compact('requests','studentName'));
     }
 
     public function requestsDisplay()
--- resources/views/classRequest.blade.php
+++ resources/views/classRequest.blade.php
@@ -9,6 +9,7 @@
     
     <link rel="stylesheet" href="{{ asset('css/editTutorProfile.css') }}">
     
+    
     <title>Class Request Form</title>
 </head>
 <body>
@@ -18,42 +19,92 @@
     @extends('studentHomeContent')
     @section('content')
    
-    <section class="home">
-   
-    <div class="container" style="margin-top:6%;  margin-left:6%; max-width: 850px;">
+    
+
+    <section class="home" style="margin-left:6%;margin-top:10%;">
+    <div class="col-sm-8 bg-white rounded-right" style="margin-top:20px; font-size:25px; margin-top:-120px;">
+                                <h3 class="mt-4 text-center " style="margin-left:2%; color: #f0db31;">Information</h3>
+                                <hr class="badge-primary mt-2 w-30">
+                                <div class="row">
+
+                                <div class="col" style="margin-left:5%; margin-top:20px;color: #fff;margin-bottom:5%;">
+                                        <p class="font-weight-bold">Name</p>@foreach ($detail as $detail)
+                                        <h6 class="text-muted">{{$detail->tutorFullName}}</h6>
+                                    
+                                    <div class="colum" style="margin-left:500px;margin-top:-60px; color: #fff;">
+                                        <p class="font-weight-bold">Subject</p>
+                                        <h6 class="text-muted">{{$detail->subject}}</h6>
+                                    </div>
+                                    </div>
+                                    </div>
+                                <div class="row" style=" margin-top:-30px;">
+                                    <div class="col" style="margin-left:5%;color: #fff;">
+                                        <p class="font-weight-bold">Email</p>
+                                        <h6 class="text-muted">{{$detail->tutorEmail}}</h6>
+                                   
+                                    <div class="col-sm-8" style="margin-left:500px;color: #fff;margin-top:-60px">
+                                        <p class="font-weight-bold">Phone</p>
+                                        <h6 class="text-muted">{{$detail->tutorPhoneNumber}}</h6>
+                                    </div>
+                                </div> 
+                               
+                                </div>
+                                <div class="row" style=" margin-top:40px;">
+                                    <div class="col-sm-8"style="margin-left:5%;color: #fff;">
+                                    <p class="font-weight-bold">Qualification</p>
+                                        <h6 class="text-muted" >{{$detail->qualification}}</h6>
+                                    </div>
+                                    @endforeach
+                                </div>
+                                <hr class="bg-primary">
+                                
+                            </div>
+                    </div>
+            </div>
+            
+
+        </div>
+</session>
+       
+        <section class="home">
+    <div class="container" style="margin-top:450px;  margin-left:6%; max-width: 850px;">
       <h1 class="form-title" style=" margin-bottom:20px;">Class Request</h1>
                 
                     <form id="" action="{{ url('/classRequestInput') }}" method="post">
                         @csrf 
                         
-           @foreach($tutorFullName as $tutorFullName)                 
+                        
         <div class="main-user-info">
           <div class="user-input-box"  style=" margin-bottom:20px;">
-            <label for="tutorName"> Tutor Name: </label>
-           
-            <input type="text"
-                    id="tutorName"
+            <label for="tutorFullName"> Tutor Name: </label>
+            @foreach($tutorFullName as $tutorFullName)   
+          <input type="text"
+                    id="tutorFullName"
                     name="tutorFullName"
-                    placeholder="" value="{{ $tutorFullName->tutorFullName }}"/>
+                    placeholder="" value="{{ $tutorFullName->tutorFullName }}" />
                     
                     @endforeach
                    
           </div>
-
+         
           <div class="user-input-box"    style=" margin-bottom:20px; ">
-            <label for="subject">Subject:</label>
-            <input type="subject"
-                    id="subject"
-                    name="subject"
-                    placeholder="subject"/>
+            <label for="tutorSubject">Subject:</label>
+            @foreach($subject as $subject) 
+            <input type="text"
+                    id="tutorSubject"
+                    name="tutorSubject"
+                    placeholder=""value="{{ $subject->tutorSubject }}"/>
+              @endforeach
           </div>
 
           <div class="user-input-box"    style=" margin-bottom:20px; justify-content: end;">
             <label for="medium">Medium :</label>
-            <input type="medium"
+            @foreach($medium as $medium) 
+           <input type="text"
                     id="medium"
                     name="medium"
-                    placeholder="medium"/>
+                    placeholder=""value="{{ $medium->tutorMedium }}"/>
+            @endforeach
           </div>
                 
          
