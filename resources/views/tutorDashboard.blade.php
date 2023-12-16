f513a70d8c4b0f52cce70e59abbe59414b3d31ee
AkeshMT <akeshmt@gmail.com>
2023-12-16 17:41:00 +0530

    tutorDashboard update

--- resources/views/tutorDashboard.blade.php
+++ resources/views/tutorDashboard.blade.php
@@ -6,7 +6,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Modern Web UI Design | By Code Info</title>
+    <title>Tutor Dashboard</title>
     <link rel="stylesheet" href="css/tutorDashboard.css">
     <!-- Font Awesome Cdn Link -->
    
@@ -72,24 +72,7 @@
                                     <div class="buttons">
                                         <a href="/editTutorProfile" class="btn">Edit</a>
                                         <input type="checkbox" id="check">
-                                        @csrf
-                                        @method('DELETE')
                                         
-
-                                        <a href="{{url('delete_profile',$profile->id)}}" ><button type="button" class="show_button" style="background: linear-gradient(to left, #525b5e 0%, #73691b 100%);margin-top:-10px;"for="check">Delete</button>
-                                    <div class="background"></div>
-                                
-                                    <div class="alert_box">
-                                        <div class="icon">
-                                            <i class="fas fa-exclamation"></i>
-                                        </div>
-                                        <header>your account will be deleted permenantly!</header>
-                                            
-                                        <p>are you sure to proceed?</p>
-                                            <div class="btns">
-                                            <label for="check">Yes, Delete!</label>
-                                            <label for="check">Cancel</label>
-                                            </div>
 
                                         </div>
                                 
@@ -434,10 +417,83 @@
 </table>
 
 </section>
-        </section>
-        </section>
+<section class="reviews-section section-padding pb-0">
+                  <div class="container">
+                      <div class="row">
+
+                          <div class="col-lg-12 col-12">
+                              <h2 class="mt-2 mb-4">Reviews</h2>
+
+                              <div class="owl-carousel reviews-carousel">
+
+                                  @if(isset($tfeedback) && count($tfeedback)>0)
+                                      @foreach ($tfeedback as $tfeedback)
+                                          <div class="reviews-thumb">
+                                              <div class="reviews-body">
+                                                  <h4>{{ $tfeedback->tutor }} - Tutor</h4>
+                                                  <h3>{{ $tfeedback->message }}</h3>
+                                              </div>
+
+                                              <div class="reviews-bottom reviews-bottom-up d-flex align-items-center">
+                                                  <div class="d-flex align-items-center justify-content-between flex-wrap w-100 ms-3">
+                                                      <p class="text-white mb-0">
+                                                          <strong>{{ $tfeedback->name }}</strong> ( <small>Student</small> )
+                                                      </p>
+
+                                                      <div class="reviews-icons">
+                                                          @for ($i = 0; $i <= 4; $i++)
+                                                          <i class="bi-star-fill{{ $i >= $tfeedback->rating ? 'active' : '' }}"></i>
+                                                          @endfor
+                                                      </div>
+                                                  </div>
+                                              </div>
+                                          </div>
+                                      @endforeach
+                                  @else
+                                      <p>No reviews found</p>
+                                  @endif
+
+                                  <!-- <div class="reviews-thumb">
+                                      <div class="reviews-body">
+                                          <h4>Tutor4u</h4>
+                                          <h3>Tutor4u is the most suitable website layout.</h3>
+                                      </div>
+
+                                      <div class="reviews-bottom reviews-bottom-up d-flex align-items-center">
+                                      
+
+                                          <div class="d-flex align-items-center justify-content-between flex-wrap w-100 ms-3">
+                                              <p class="text-white mb-0">
+                                                  <strong>Sandy</strong>, <small>CEO</small>
+                                              </p>
+
+                                              <div class="reviews-icons">
+                                                  <i class="bi-star-fill"></i>
+                                                  <i class="bi-star-fill"></i>
+                                                  <i class="bi-star-fill"></i>
+                                                  <i class="bi-star-fill"></i>
+                                                  <i class="bi-star-fill"></i>
+                                              </div>
+                                          </div>
+                                      </div>
+                                  </div> -->
+
+                              </div>
+                              
+                          </div>
+
+                      </div>
+                  </div>
+                  
+                  <!-- <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#36363e" fill-opacity="1" d="M0,96L40,117.3C80,139,160,181,240,186.7C320,192,400,160,480,149.3C560,139,640,149,720,176C800,203,880,245,960,250.7C1040,256,1120,224,1200,229.3C1280,235,1360,277,1400,298.7L1440,320L1440,320L1400,320C1360,320,1280,320,1200,320C1120,320,1040,320,960,320C880,320,800,320,720,320C640,320,560,320,480,320C400,320,320,320,240,320C160,320,80,320,40,320L0,320Z"></path></svg>  -->
+                  
+                  
+              </section>
+        </section>                 
 </div>
-    
+
+
+        </section> 
 
 </div>
       </section>
