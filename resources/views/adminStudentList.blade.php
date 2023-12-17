cceb2e6396a3eaa92803053ce59291ef9bf4b99e
GayathtriS <kpgsewwandi1998@gmail.com>
2023-12-18 01:21:39 +0700

    create back buton to admin dashboard

--- resources/views/adminStudentList.blade.php
+++ resources/views/adminStudentList.blade.php
@@ -14,7 +14,33 @@
     <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
     rel="stylesheet"/>
     
-
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <style>
+          
+        
+        button,.fa{
+            color: #383838;
+            box-shadow: 3px 3px 6px rgba(0, 0, 0, 0.5);
+            cursor: pointer;
+        }
+        .fa{
+            margin-left: -10px;
+            font-size: 20px;
+            padding: 10px 18px;
+        }
+        #back,.fa-mail-forward{
+            border-top-right-radius: 20px;
+            border-bottom-right-radius: 20px;
+        }
+        
+        
+        .fa-mail-reply{
+            background: white;
+        }
+        
+    </style>
+        
     <title>Student Table</title>
 
 </head>
@@ -41,10 +67,13 @@
     @section('content')
     <section class="home">
     <header class="header">
+        
       <div class="header_logo">
-        <a href="#"style="color:#fff;  font-weight: bold; font-size:25px;">Student List</a>
+      <a href="/adminDashboard">
+            <i class="fa fa-mail-reply" aria-hidden="true"></i></a>
+        <a href="#"style="color:#fff; margin-left:30px; font-weight: bold; font-size:25px;">Student List</a>
       </div>
-  
+        
       
   
       
@@ -55,7 +84,7 @@
     <div class="main_body">
         <div class="container">
             
-     
+        
         <div class="row d-flex justify-content-center">
                 <div class="col-md-11 mt-5 pt-5">
                     <div class="row z-depth-3">
