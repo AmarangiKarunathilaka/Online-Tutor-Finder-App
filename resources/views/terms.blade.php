62f6fd89e8538fca86e3449dd2bf84bd1f2e8bf5
KavindraHerath <rekhankavindra@gmail.com>
2023-12-18 00:15:17 +0530

    Update Terms and conditions

--- resources/views/terms.blade.php
+++ resources/views/terms.blade.php
@@ -23,7 +23,19 @@
 
         <link rel="stylesheet" href="css/registrationstyle.css">
        
-        
+        <style>
+        /* You can apply styles directly to the img tag */
+        img {
+            width: 5px; /* You can adjust this percentage based on your needs */
+            height: auto; /* This ensures that the aspect ratio is maintained */
+        }
+
+        /* Alternatively, you can use a class */
+        .resized-image {
+            width:5px; /* You can adjust this percentage based on your needs */
+            height: auto; /* This ensures that the aspect ratio is maintained */
+        }
+    </style>
 
     </head>
     
@@ -32,9 +44,9 @@
     <nav class="navbar navbar-expand-lg  fixed-top shadow-lg">
        
        <div class="container">
-           
+           <div>
            <img src="images/f.png" alt="" />
-           
+           </div>
 
            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                <span class="navbar-toggler-icon"></span>
@@ -69,9 +81,61 @@
                     
                   <h2><div class="row justify-content-center">Terms and Conditions </div></h2>
                
-               <h4>1. An Intellectual Property clause will inform users that the contents, logo and other visual media you created is your property and is protected by copyright laws.<br><br>
-
-                2. A Termination clause will inform users that any accounts on your website and mobile app, or users' access to your website and app, can be terminated in case of abuses or at your sole discretion.</h4>       
+               <h4>1. Account Registration and Usage
+Register an accurate account and update information promptly.
+Authorize inquiries for verification.
+Adhere to client policies.
+Safeguard account information.
+Do not impersonate or share account details.
+Notify Teach Away of any unauthorized account use. <br><br>
+2. Representations and Warranties
+Have the required knowledge for teaching English.
+Utilize paid time solely for providing tutoring services. <br><br>
+3. Interactions with Users and Provision of Services
+Teach Away and the Platform facilitate tutoring services but do not control or supervise.
+Tutors are responsible for interactions and must comply with laws.
+Screen users at your discretion and report issues to Teach Away.
+Adhere to laws and regulations.
+Maintain quality, appropriateness, and legality in provided content.
+Respect user privacy and use the platform appropriately. <br><br>
+4. Appearance and Setting
+Maintain a professional appearance during sessions.
+Follow guidelines for background, lighting, sound, and video quality.
+Adhere to login protocols. <br><br>
+5. Fees
+Fees are paid per hour, inclusive of taxes.
+Penalty costs may apply for lateness, unfinished sessions, or no-shows.
+Payment via PayPal, with responsibility for PayPal fees. <br><br>
+6. No Endorsement
+Teach Away does not endorse the platform or users.
+Teach Away is not liable for damages resulting from interactions. <br><br>
+7. Confidentiality and Intellectual Property
+Agree to a confidentiality and proprietary information agreement. <br><br>
+8. Privacy
+Acknowledge and agree to the Teach Away Privacy Policy. <br><br>
+9. Indemnity
+Tutors indemnify Teach Away against claims arising from services provided. <br><br>
+10. Limitation of Liability
+Teach Away and the client are not liable for certain damages.
+Teach Away's aggregate liability is limited. <br><br>
+11. Termination
+Teach Away may terminate the agreement for various reasons.
+Tutors may terminate with notice.
+Certain obligations survive termination. <br><br>
+12. Obligations Surviving Termination
+Certain obligations regarding confidentiality and intellectual property persist after termination. <br><br>
+13. Miscellaneous
+The agreement constitutes the entire understanding.
+Satisfaction of claims is in lieu of other entitlements.
+No waiver of defaults.
+The agreement is binding. <br> <br>
+14. CONFIDENTIALITY AND PROPRIETARY INFORMATION AGREEMENT
+Agreement regarding confidential information and proprietary property.
+Tutors agree not to use or disclose third-party trade secrets.
+Tutors assign ownership of proprietary property to Teach Away.
+Tutors waive moral rights and agree to execute additional agreements if required.
+Tutors acknowledge unique services, agree to monetary damages for breaches, and acknowledge Teach Away's right to seek injunctions for breaches.
+This summary is not exhaustive, and it's advisable to refer to the original document for complete details. <br><br></h4>       
                     
                          
             </section>              
